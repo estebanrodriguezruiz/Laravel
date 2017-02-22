d4db3dd0e7b8708482946cebe43f15995d364887
number1 <estebanjrr5@gmail.com>
2017-02-22 17:20:09 +0100

    Prueba commit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Mi proyecto
                 </div>
 
                 <div class="links">
